c2150f7ff32c465c15659a5caaf667da6e8d5b43
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-09-01 11:31:32 +0100

    Add project files.

---
+++ File_IO_Assignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace File_IO_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick a number:\n");
+            var userInput = Console.ReadLine();
+            using (StreamWriter file = new StreamWriter(@"C:\Users\will2\source\repos\File_IO_Assignment\File_IO_Assignment\log.txt", true))
+            {
+                file.WriteLine(userInput);
+            }
+
+            var text = File.ReadAllText(@"C:\Users\will2\source\repos\File_IO_Assignment\File_IO_Assignment\log.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
